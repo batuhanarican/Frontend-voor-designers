416759e06f531247f4e40ca68f67cb7df587b845
batuhanarican <batuhanarican@hotmail.com>
2018-03-04 17:58:11 +0100

    Opdr2

--- Opdr2/v01/js/script.js
+++ Opdr2/v01/js/script.js
@@ -5,13 +5,15 @@ var section1 = document.querySelector('div.section1');
 var section2 = document.querySelector('div.section2');
 var section3 = document.querySelector('div.section3');
 
+var indicator = document.querySelectorAll('nav ul li');
+
 var w = window.innerWidth;
 var h = window.innerHeight;
 
-function countHeight(){
-    counter.innerHTML="Offset = " + window.pageYOffset;   
-    counter2.innerHTML="Height = " + window.innerHeight;
-    counter3.innerHTML="Width = " + window.innerWidth;
+function countHeight() {
+    counter.innerHTML = "Offset = " + window.pageYOffset;   
+    counter2.innerHTML = "Height = " + window.innerHeight;
+    counter3.innerHTML = "Width = " + window.innerWidth;
     w = window.innerWidth;
     h = window.innerHeight;
 }
@@ -21,28 +23,46 @@ function measureScreen (){
     console.log(h);
 }
 
-function changeColor(){
+function changeColor() {
   var height = window.pageYOffset;
-    if(height >= h/2){
+    if(height >= h/2) {
         section1.classList.add('section1add');
+        indicator[0].classList.remove('indicator1');
   }
     if(height < h/2){
         section1.classList.remove('section1add');
+        indicator[0].classList.add('indicator1');
+        indicator[1].classList.remove('indicator2');
   }
-    if(height >= h-(h/2)){
+    if(height >= h-(h/2)) {
         section2.classList.add('section2add');
+        indicator[1].classList.add('indicator2');
+        indicator[2].classList.remove('indicator3');
   }
-    if(height < h-(h/2)){
+    if(height < h-(h/2)) {
         section2.classList.remove('section2add');
   }
-    if(height >= h*2-(h/2)){
+    if(height >= h*2-(h/2)) {
         section3.classList.add('section3add');
         section2.classList.remove('section2add');
+        indicator[1].classList.remove('indicator2');
+        indicator[2].classList.add('indicator3');
   }
-    if(height < h*2-(h/2)){
+    if(height < h*2-(h/2)) {
         section3.classList.remove('section3add');
   }
 }
+
+function smoothScroll(x) {
+    console.log('Scroll smooth ' + (x+1));
+    window.scroll({
+    top: h*x, 
+    left: 0, 
+    behavior: 'smooth' 
+});
+}
+
+countHeight();
 
 console.log(window.scrollY);
 console.log(w);
@@ -52,3 +72,13 @@ window.addEventListener('scroll', countHeight);
 window.addEventListener('scroll', changeColor);
 window.addEventListener('resize', measureScreen);
 window.addEventListener('resize', countHeight);
+
+indicator[0].addEventListener('click', function(){
+    smoothScroll(0);
+}, false);
+indicator[1].addEventListener('click', function(){
+    smoothScroll(1);
+}, false);
+indicator[2].addEventListener('click', function(){
+    smoothScroll(2);
+}, false);
---
+++ Opdr2/v02/js/script.js
@@ -0,0 +1,76 @@
+var counter = document.querySelector('.height-offset');
+var counter2 = document.querySelector('.height-counter');
+var counter3 = document.querySelector('.width-counter');
+
+var chameleon = document.querySelector('.chameleon');
+
+var indicator = document.querySelectorAll('nav ul li');
+
+var w = window.innerWidth;
+var h = window.innerHeight;
+
+function countHeight() {
+    counter.innerHTML = "Offset = " + window.pageYOffset;   
+    counter2.innerHTML = "Height = " + window.innerHeight;
+    counter3.innerHTML = "Width = " + window.innerWidth;
+    w = window.innerWidth;
+    h = window.innerHeight;
+}
+
+function measureScreen (){
+    console.log(w);
+    console.log(h);
+}
+
+function changeColor() {
+  var height = window.pageYOffset;
+    if(height >= h/2) {
+        indicator[0].classList.remove('indicator1');
+  }
+    if(height < h/2){
+        indicator[0].classList.add('indicator1');
+        indicator[1].classList.remove('indicator2');
+  }
+    if(height >= h-(h/2)) {
+        indicator[1].classList.add('indicator2');
+        indicator[2].classList.remove('indicator3');
+  }
+    if(height < h-(h/2)) {
+  }
+    if(height >= h*2-(h/2)) {
+        indicator[1].classList.remove('indicator2');
+        indicator[2].classList.add('indicator3');
+  }
+    if(height < h*2-(h/2)) {
+  }
+}
+
+function smoothScroll(x) {
+    console.log('Scroll smooth ' + (x+1));
+    window.scroll({
+    top: h*x, 
+    left: 0, 
+    behavior: 'smooth' 
+});
+}
+
+countHeight();
+
+console.log(window.scrollY);
+console.log(w);
+console.log(h);
+    
+window.addEventListener('scroll', countHeight);
+window.addEventListener('scroll', changeColor);
+window.addEventListener('resize', measureScreen);
+window.addEventListener('resize', countHeight);
+
+indicator[0].addEventListener('click', function(){
+    smoothScroll(0);
+}, false);
+indicator[1].addEventListener('click', function(){
+    smoothScroll(1);
+}, false);
+indicator[2].addEventListener('click', function(){
+    smoothScroll(2);
+}, false);
