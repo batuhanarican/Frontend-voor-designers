fe2edd573e67b95aeaf5187776e1c18e173e6752
batuhanarican <batuhanarican@hotmail.com>
2018-04-10 17:37:52 +0200

    Update

--- Opdr3/v03/js/javascript.js
+++ Opdr3/v03/js/javascript.js
@@ -32,7 +32,7 @@ var pFeatured = document.querySelector('main div.featured p');
 
 //******************Request******************
 
-var requestURL = './movies.json';
+var requestURL = 'https://batuhanarican.github.io/Frontend-voor-designers/Opdr3/v03/movies.json';
 var request = new XMLHttpRequest();
 
 request.open('GET', requestURL);
